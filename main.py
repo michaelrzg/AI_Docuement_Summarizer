95290bf51779d9f7df61dc45115e9e4ad892908d
michaelrzg <michaelrizig@gmail.com>
2024-11-22 00:19:06 -0500

    added comments

--- main.py
+++ main.py
@@ -101,10 +101,14 @@ def abstractive_BERT_BART(text):
 
 # MAIN
 
+# input article from npr
 input = "KYIV, Ukraine — Russia fired an experimental intermediate-range ballistic missile at Ukraine overnight, Russian President Vladimir Putin said in a TV speech Thursday, warning that the Kremlin could use it against military installations of countries that have allowed Ukraine to use their missiles to strike inside Russia. Putin said the new missile, called \"Oreshnik,\" Russian for \"hazel,\" used a nonnuclear warhead. Ukraine's air force said a ballistic missile hit the central Ukrainian city of Dnipro, saying it was launched from the Astrakhan region in southeastern Russia, more than 770 miles away. Ukrainian officials said it and other rockets damaged an industrial facility, a rehabilitation center for people with disabilities and residential buildings. Three people were injured, according to regional authorities. \"This is an obvious and serious increase in the scale and brutality of this war,\" Ukrainian President Volodymyr Zelenskyy wrote on his Telegram messaging app. The attack came during a week of intense fighting in the nearly three years of war since Russia invaded Ukraine, and it followed U.S. authorization earlier this week for Ukraine to use its sophisticated weapons to strike targets deep inside Russia. Putin said Ukraine had carried out attacks in Russia this week using long-range U.S.-made Army Tactical Missile System (ATACMS) and British-French Storm Shadow missiles. He said Ukraine could not have carried out these attacks without NATO involvement. \"Our test use of Oreshnik in real conflict conditions is a response to the aggressive actions by NATO countries towards Russia,\" Putin said. He also warned: \"We believe that we have the right to use our weapons against military facilities of the countries that allow to use their weapons against our facilities.\""
+# Extactive Summary
 print("\n\nExtractive Summary: \n")
 print(extractive(input,3))
+# Abstractive Summary with BART
 print("\n\nAbstractive Summary with BART: ")
 print(abstractive_BART(input) + "\n")
+# Abstractive Summary with BERT Encodings and BART Decoding
 print("\n\nAbstractive Summary with BERT + BART: ")
 print(abstractive_BERT_BART(input) + "\n")
